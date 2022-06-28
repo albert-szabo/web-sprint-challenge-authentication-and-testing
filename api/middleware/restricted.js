fd8e6906e33b899a22119e554b8c3b670581b6de
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-28 11:40:38 -0700

    Completed restricted function / file to limit access

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,29 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets/index');
 
-    1- On valid token in the Authorization header, call next.
+/*
+  IMPLEMENT
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
+  1- On valid token in the Authorization header, call next.
 
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  2- On missing token in the Authorization header,
+    the response body should include a string exactly as follows: "token required".
+
+  3- On invalid or expired token in the Authorization header,
+    the response body should include a string exactly as follows: "token invalid".
+*/
+
+module.exports = (request, response, next) => {
+  const token = request.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: 'token required' });
+  }
+  jwt.verify(token, JWT_SECRET, (error, decodedToken) => {
+    if (error) {
+      next({ status: 401, message: 'token invalid' });
+    } else {
+      request.decodedToken = decodedToken;
+      next();
+    }
+  })
 };
